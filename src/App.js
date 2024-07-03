0b677e4e9f64080a3735df5b197647e9c0d005c0
Sesughun <sesughunlan@gmail.com>
2024-07-03 09:47:07 -0700

    flipping flash cards with no api, just sample questions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import FlashcardList from "./Components/FlashcardList";
 
 function App() {
-  return <div className="App"></div>;
+  const [flashcards, setFlashcards] = useState(sample);
+  return (
+    <div className="App">
+      <FlashcardList flashcards={flashcards} />
+    </div>
+  );
 }
 
+const sample = [
+  {
+    id: 1,
+    question: " Value of 2+2",
+    answer: "4",
+    options: ["2", "3", "4", "5"],
+  },
+  {
+    id: 2,
+    question: " Value of 3+2",
+    answer: "5",
+    options: ["2", "3", "4", "5"],
+  },
+];
 export default App;
